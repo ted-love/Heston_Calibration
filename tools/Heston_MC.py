6fcc27fd1ff4d69babc7b6c92a69738456a9a03a
ted-love <46618315+ted-love@users.noreply.github.com>
2023-11-06 16:48:47 +1100

    Add files via upload

--- tools/Heston_MC.py
+++ tools/Heston_MC.py
@@ -7,7 +7,7 @@ Created on Sun Jul 30 12:04:29 2023
 """
 import numpy as np
 
-def heston_model(kappa, omega, N, Nsims, V0, S0, p, r, K, T,theta):
+def heston_MC(kappa, omega, N, Nsims, V0, S0, rho, r, K, T, theta, flag):
     # initialise other parameters
     dt = T/N
 
@@ -19,25 +19,26 @@ def heston_model(kappa, omega, N, Nsims, V0, S0, p, r, K, T,theta):
     
     Z_v = np.random.normal(0,1,[Nsims,N])
     Z_2 = np.random.normal(0,1,[Nsims,N])
-    Z_s = p*Z_v + np.sqrt(1-(p**2))*Z_2
+    Z_s = rho*Z_v + np.sqrt(1-(rho**2))*Z_2
     
     V = np.full(shape=(Nsims,N+1),fill_value=V0)
     V_tilde = V
 
     S = np.full(shape=(Nsims,N+1),fill_value=S0)
+
  
     for i in range(1,N+1):
-
+        
         S[:,i] = S[:,i-1]*np.exp((r-(0.5*V[:,i-1]))*dt + np.sqrt(V[:,i-1])*np.sqrt(dt)*Z_s[:,i-1])
         
         V_tilde[:,i] = f1(V_tilde[:,i-1]) + kappa*dt*(theta - f2(V_tilde[:,i-1])) \
             + omega*np.sqrt(f3(V_tilde[:,i-1]))*Z_v[:,i-1]*np.sqrt(dt)
             
         V[:,i] = f3(V_tilde[:,i])
-        
     
-    C = np.exp(-r*T)*np.maximum(S[:,-1]-K,0)
-    C = np.mean(C)
-
-    return C
-
+    if flag=='c':
+        return np.mean(np.exp(-r*T)*np.maximum(S[:,-1]-K,0))
+    
+    elif flag=='p':
+        return np.mean(np.exp(-r*T)*np.maximum(K-S[:,-1],0))
+ 
